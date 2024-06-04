63b613213a3e291722980ac669ae5884b7ab9b05
wendybarrantes <wenbarrantes06@gmail.com>
2024-06-04 15:45:26 -0600

    logica POST

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-// Inserte el código aquí
+let tareaAg = document.getElementById("tarea")
+let btnAgg = document.getElementById("btnAgg")
 
+async function postData() {
+    console.log("dsda");
+    try {
+        let tarea = {
+            nombre: tareaAg.value,
+            id: Date.now(),
+            estado: false
+        }
+        let peticion = await fetch('http://localhost:3000/api/task',{
+        method:"POST",  
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          },
+          body: JSON.stringify(tarea)
+        })
+        let guardarDatos= await peticion.json()
+        console.log(guardarDatos);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+btnAgg.addEventListener("click",postData)
