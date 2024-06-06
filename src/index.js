63dfef35eed52380af13e58f7398607ca46257da
wendybarrantes <wenbarrantes06@gmail.com>
2024-06-06 02:52:43 -0600

    documentacion y metodo DELETE

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-let tareaAg = document.getElementById("tarea")
-let btnAgg = document.getElementById("btnAgg")
-let contenedor = document.getElementById("contenedorTareas")
+/* 
+Referenciar las etiquetas de HTML para darle funcionalidad en js.
+*/
+let inputTask = document.getElementById("inputTask")
+let BtnAT = document.getElementById("BtnAT")
+let ContainerTask = document.getElementById("ContainerTask")
+let ContTask= document.getElementById("ContTask")
 
-//POST
+/*
+Método POST.
+el método POST se crea con una función asíncrona que tiene un bloque 
+try-catch. Dentro de la función se crea un objeto que va a ser el cuerpo
+de la Api(enlace). Se controla la petición a la Api con el fetch
+indicando que es un método POST luego se le pasa al body el objeto convertido en formaro JSON.
+*/
 async function postData() {
-    console.log("dsda");
     try {
         let tarea = {
-            nombre: tareaAg.value,
+            nombre: inputTask.value,
             id: Date.now(),
             estado: false
         }
@@ -25,7 +34,13 @@ async function postData() {
     }
 }
 
-//GET
+/*El método GET. Se crea con una función asíncrona, que llama a la Api para traer los datos. 
+Estos datos se guardan dentro de la variable datos, está variable es igual al await de nuestra petición, o sea 
+los datos que haya traído de la Api .
+
+La variable datos funciona como arreglo por eso se le aplica la función forEach, esta va a iterar sobre cada tarea 
+que haya en nuestra Api haciendo el código que esté dentro del bloque forEach.
+*/
 async function getData() {
     let peticion = await fetch('http://localhost:3000/api/task')
     let datos = await peticion.json()
@@ -39,9 +54,26 @@ async function getData() {
         h2.appendChild (checkBox)
         h2.appendChild(deleteBtn)
         div.appendChild(h2)
-
+        ContainerTask.appendChild(div)
+        deleteBtn.addEventListener("click",()=>{
+         deleteTask(tarea.id)
+        })
     });
 }
 btnAgg.addEventListener("click",postData)
+
+
+/*Metodo Delete. creamos una función asímcrona que recibe por parametro el id de la tarea a eliminar 
+dentro de esta función se crea una variable petición que hace un await.fetch pasandole a la 
+url(enlace) el id y luego ejecutando el método delete.
+*/
+
+async function deleteTask(id) {
+    let peticion = await fetch(`http://localhost:3000/api/task/${id}`,{
+        method: "DELETE" 
+    })
+console.log("se borró la tarea" + id);
+
+}
 
 
